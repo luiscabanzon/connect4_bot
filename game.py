1dd99f9fd6b6e1f0cae6a08bfd6e30537c08ebf1
luiscabanzon <luiscabanzon@gmail.com>
2018-01-15 00:01:05 +0100

    Added: check_victory function

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+
 print(sys.version)
 
 BOARD_WIDTH = 7
@@ -19,11 +21,32 @@ def check_end():
     return False
 
 
+def check_victory(space, player):
+    # Space: 2-D array (not matrix!)
+    pattern = str(player) * 4
+    diagonals = np.max([BOARD_HEIGHT, BOARD_WIDTH])
+    space = [np.pad(s, pad_width=(0, diagonals - len(s)), mode='constant', constant_values=3) for s in space]
+    space = np.matrix(space, dtype=np.int32)
+    space_flip = np.fliplr(space)
+    print(space)
+    for i in range(diagonals * -1, diagonals):
+        if pattern in "".join([str(s) for s in space.diagonal(i).tolist()[0]]) \
+                or pattern in "".join([str(s) for s in space_flip.diagonal(i).tolist()[0]]):
+            return True
+    for row in space.tolist():
+        if pattern in "".join([str(r) for r in row]):
+            return True
+    for column in space.T.tolist():
+        if pattern in "".join([str(c) for c in column]):
+            return True
+    return False
+
+
 def print_board():
     for column in board:
         print(' '.join([str(c) for c in column]))
 
-players = [0,1]
+players = [0, 1]
 turn = 0
 
 while True:
@@ -31,7 +54,9 @@ while True:
     action = int(input("Choose column : "))
     play_turn(action, player)
     print_board()
+    if check_victory(board, player):
+        print("Player %s WINS!" % player)
+        break
     if check_end():
         break
     turn += 1
-
